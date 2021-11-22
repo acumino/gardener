9be3edd60c229c636a61d771e20b085c5d9babe3
acumino <sksgkpvks@gmail.com>
2021-11-22 15:25:33 +0530

    Updated monitoring test

--- pkg/operation/botanist/component/nodelocaldns/monitoring_test.go
+++ pkg/operation/botanist/component/nodelocaldns/monitoring_test.go
@@ -15,10 +15,61 @@
 package nodelocaldns_test
 
 import (
+	"github.com/gardener/gardener/pkg/operation/botanist/component"
 	. "github.com/gardener/gardener/pkg/operation/botanist/component/nodelocaldns"
+	"github.com/gardener/gardener/pkg/operation/botanist/component/test"
 
 	. "github.com/onsi/ginkgo"
 )
 
 var _ = Describe("Monitoring", func() {
+	var component component.MonitoringComponent
+
+	BeforeEach(func() {
+		component = New(nil, "", Values{})
+	})
+
+	It("should successfully test the scrape config", func() {
+		test.ScrapeConfigs(component, expectedScrapeConfig)
+	})
 })
+
+const (
+	expectedScrapeConfig = `job_name: node-local-dns
+ scheme: https
+ tls_config:
+   insecure_skip_verify: true
+   cert_file: /etc/prometheus/seed/prometheus.crt
+   key_file: /etc/prometheus/seed/prometheus.key
+ honor_labels: false
+ kubernetes_sd_configs:
+ - role: pod
+   api_server: https://kube-apiserver:443
+   tls_config:
+     ca_file: /etc/prometheus/seed/ca.crt
+     cert_file: /etc/prometheus/seed/prometheus.crt
+     key_file: /etc/prometheus/seed/prometheus.key
+ relabel_configs:
+ - source_labels:
+   - __meta_kubernetes_pod_name
+   action: keep
+   regex: node-local.*
+ - source_labels:
+   - __meta_kubernetes_pod_container_name
+   - __meta_kubernetes_pod_container_port_name
+   action: keep
+   regex: node-cache;metrics
+ - source_labels: [ __meta_kubernetes_pod_name ]
+   target_label: pod
+ - target_label: __address__
+   replacement: kube-apiserver:443
+ - source_labels: [__meta_kubernetes_pod_name,__meta_kubernetes_pod_container_port_number]
+   regex: (.+);(.+)
+   target_label: __metrics_path__
+   replacement: /api/v1/namespaces/kube-system/pods/${1}:${2}/proxy/metrics
+ metric_relabel_configs:
+ - source_labels: [ __name__ ]
+   action: keep
+   regex: ^(coredns_build_info|coredns_cache_entries|coredns_cache_hits_total|coredns_cache_misses_total|coredns_dns_request_duration_seconds_count|coredns_dns_request_duration_seconds_bucket|coredns_dns_responses_total|coredns_forward_requests_total|coredns_forward_responses_total|coredns_kubernetes_dns_programming_duration_seconds_bucket|coredns_kubernetes_dns_programming_duration_seconds_count|coredns_kubernetes_dns_programming_duration_seconds_sum|process_max_fds|process_open_fds)$
+ `
+)
