8ebf085f217666489d210c71cabf60c33ab957e5
Sonu Kumar Singh <sksgkpvks@gmail.com>
2024-11-06 15:46:32 +0530

    Add feature-gate `InPlaceNodeUpdates`

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -59,6 +59,11 @@ const (
 	// owner: @oliver-goetz
 	// alpha: v1.109
 	NodeAgentAuthorizer featuregate.Feature = "NodeAgentAuthorizer"
+
+	// InPlaceNodeUpdates enables setting the update strategy of worker pools to `AutoInPlaceUpdate` or `ManualInPlaceUpdate` in Shoot API.
+	// owner: @acumino @ary1992 @shafeeqes
+	// alpha: v1.111.0
+	InPlaceNodeUpdates featuregate.Feature = "InPlaceNodeUpdates"
 )
 
 // DefaultFeatureGate is the central feature gate map used by all gardener components.
@@ -93,6 +98,7 @@ var AllFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	NewWorkerPoolHash:         {Default: false, PreRelease: featuregate.Alpha},
 	NewVPN:                    {Default: false, PreRelease: featuregate.Alpha},
 	NodeAgentAuthorizer:       {Default: false, PreRelease: featuregate.Alpha},
+	InPlaceNodeUpdates:        {Default: false, PreRelease: featuregate.Alpha},
 }
 
 // GetFeatures returns a feature gate map with the respective specifications. Non-existing feature gates are ignored.
