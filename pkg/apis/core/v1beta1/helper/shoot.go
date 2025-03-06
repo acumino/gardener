94356872b7a35d76756658b12a67be21c2aa11aa
Shafeeque E S <shafeeque.e.s@sap.com>
2025-03-06 21:09:33 +0530

    Add helper functions

--- pkg/apis/core/v1beta1/helper/shoot.go
+++ pkg/apis/core/v1beta1/helper/shoot.go
@@ -667,6 +667,24 @@ func IsUpdateStrategyInPlace(updateStrategy *gardencorev1beta1.MachineUpdateStra
 	return *updateStrategy == gardencorev1beta1.AutoInPlaceUpdate || *updateStrategy == gardencorev1beta1.ManualInPlaceUpdate
 }
 
+// IsUpdateStrategyAutoInPlace returns true if the given machine update strategy is AutoInPlaceUpdate.
+func IsUpdateStrategyAutoInPlace(updateStrategy *gardencorev1beta1.MachineUpdateStrategy) bool {
+	if updateStrategy == nil {
+		return false
+	}
+
+	return *updateStrategy == gardencorev1beta1.AutoInPlaceUpdate
+}
+
+// IsUpdateStrategyManualInPlace returns true if the given machine update strategy is ManualInPlaceUpdate.
+func IsUpdateStrategyManualInPlace(updateStrategy *gardencorev1beta1.MachineUpdateStrategy) bool {
+	if updateStrategy == nil {
+		return false
+	}
+
+	return *updateStrategy == gardencorev1beta1.ManualInPlaceUpdate
+}
+
 // IsShootIstioTLSTerminationEnabled returns true if the Istio TLS termination for the shoot kube-apiserver is enabled.
 func IsShootIstioTLSTerminationEnabled(shoot *gardencorev1beta1.Shoot) bool {
 	shootKubernetesVersion, err := semver.NewVersion(shoot.Spec.Kubernetes.Version)
